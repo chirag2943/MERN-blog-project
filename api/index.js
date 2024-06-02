a77dfdb612d6f5409d7f2a58093086b64a19124f
chirag2943 <chiragchouhan866@gmail.com>
2024-06-02 19:37:58 +0530

    created api route

--- api/index.js
+++ api/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
 mongoose
+// eslint-disable-next-line no-undef
 .connect(process.env.MONGO)
 .then(()=>{
     console.log("connected")
@@ -14,8 +15,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
