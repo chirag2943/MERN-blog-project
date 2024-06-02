4f7d28c356c6458aa1b3dc7f338a74c6b75e0516
chirag2943 <chiragchouhan866@gmail.com>
2024-06-02 22:10:06 +0530

    added middleware and function to handle error

--- api/index.js
+++ api/index.js
@@ -23,3 +23,14 @@ app.listen(3000, () => {
 
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
+
+app.use((err,req,res,next) => {
+    const statuscode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    res.status(statuscode).json({
+        sucess: false,
+        statuscode,
+        message
+    })
+
+})
