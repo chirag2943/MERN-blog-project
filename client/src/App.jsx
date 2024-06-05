d29a03d95cfca22ba2b691732010a7fe3ed3b910
chirag2943 <chiragchouhan866@gmail.com>
2024-06-06 00:35:09 +0530

    created foter component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,13 @@ import Projects from './pages/Projects'
 import SignIn from './pages/SignIn' 
 import SignUp from './pages/SignUp' 
 import Header from './components/Header'
+import Footer from './components/Footer'
 
-export default function App() {
+
+export function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header/>
        <Routes>
          <Route path="/" element={<Home />} />
          <Route path="/about" element={<About />} />
@@ -19,6 +21,7 @@ export default function App() {
          <Route path="/sign-in" element={<SignIn />} />
          <Route path="/sign-up" element={<SignUp />} />
        </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
