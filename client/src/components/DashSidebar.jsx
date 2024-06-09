045c4c00d52911572af1129fff5f427ab76cc1ac
chirag2943 <chiragchouhan866@gmail.com>
2024-06-10 00:21:53 +0530

    added user profile upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -18,15 +18,22 @@ export default function DashSidebar() {
     }, [location.search]);
 
   return (
-    <Sidebar className="w-full md:w-56">
+    <Sidebar className="w-full md:w-56 ">
          <Sidebar.Items>
             <Sidebar.ItemGroup>
             <Link to='/dashboard?tab=profile'>
-                <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"user"} labelcolor = "dark">
+                <Sidebar.Item 
+                  active={tab === 'profile'} 
+                  icon={HiUser} 
+                  label={"user"} 
+                  labelcolor = "dark" 
+                  as='div' >
                     Profile
                 </Sidebar.Item>
             </Link>
-                <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer'>
+                <Sidebar.Item  
+                  icon={HiArrowSmRight} 
+                  className='cursor-pointer'>
                     Sign Out 
                 </Sidebar.Item>
             </Sidebar.ItemGroup>
