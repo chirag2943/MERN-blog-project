5eda93e6722b491a78718797b8292d0a1dcd2de7
chirag2943 <chiragchouhan866@gmail.com>
2024-06-12 23:52:17 +0530

    added admin functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
         type:String,
         default: "https://imgs.search.brave.com/xRLAOStYtw4m5_8Pov_PBYMh7lmcrkLzavkwcT3lVHU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAwLzY0LzY3LzUy/LzM2MF9GXzY0Njc1/MjA5Xzd2ZTJYUUFO/dXp1SGpNWlhQM2FJ/WUlwc0RLRWJGNWRE/LmpwZw",
     },
+    isAdmin:{
+        type:Boolean,
+        default:false,
+    },
 },{timestamps: true}
 );
 
