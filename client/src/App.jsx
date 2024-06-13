63eb9b3fd3ed60e47a7383b999b8f2e16a5edd78
chirag2943 <chiragchouhan866@gmail.com>
2024-06-13 18:07:59 +0530

    added create-post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import SignUp from './pages/SignUp'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import PrivateRoute from './components/PrivateRoute'
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
+import CreatePost from './pages/CreatePost'
 
 
 export function App() {
@@ -19,7 +21,10 @@ export function App() {
          <Route path="/about" element={<About />} />
          <Route path="/projects" element={<Projects />} />
          <Route element={<PrivateRoute/>}>
-         <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+         </Route>
+         <Route element={<OnlyAdminPrivateRoute/>}>
+          <Route path="/create-post" element={<CreatePost/>} />
          </Route>
          <Route path="/sign-in" element={<SignIn />} />
          <Route path="/sign-up" element={<SignUp />} />
