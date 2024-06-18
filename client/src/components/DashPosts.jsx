10febefc8875f6588cb2ca9b488fab32a6e6c353
chirag2943 <chiragchouhan866@gmail.com>
2024-06-18 20:23:19 +0530

    added show more functionality to posts in dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
 	const { currentUser } = useSelector((state) => state.user);
 	const [userPosts, setUserPosts] = useState([]);
+	const [showMore, setShowMore] = useState(true);
 	console.log(userPosts);
 	useEffect(() => {
 		const fetchPosts = async () => {
@@ -15,6 +16,9 @@ export default function DashPosts() {
 				const data = await res.json();
 				if (res.ok) {
 					setUserPosts(data.posts);
+					if (data.posts.length < 9) {
+						setShowMore(false);
+					}
 				}
 			} catch (error) {
 				console.log(error.message);
@@ -25,6 +29,23 @@ export default function DashPosts() {
 			fetchPosts();
 		}
 	}, [currentUser._id]);
+
+	const handleShowMore = async () => {
+		const startIndex = userPosts.length;
+		try {
+			const res = await fetch(
+				`/api/post/getposts?userID=${currentUser._id}&startIndex=${startIndex}`
+			);
+			const data = await res.json();
+			if (res.ok) {
+				setUserPosts((prev) => [...prev, ...data.posts]);
+				if (data.posts.length < 9) {
+					setShowMore(false);
+				}
+			}
+		} catch (error) {}
+	};
+
 	return (
 		<div
 			className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar
@@ -84,6 +105,13 @@ export default function DashPosts() {
 							</Table.Body>
 						))}
 					</Table>
+					{showMore && (
+						<button
+							onClick={handleShowMore}
+							className="w-full text-teal-500 self-center text-sm py-7">
+							Show More
+						</button>
+					)}
 				</>
 			) : (
 				<p>you have no posts yet!</p>
