00fe28a97fc018d3c802adb61d392af421c10f16
chirag2943 <chiragchouhan866@gmail.com>
2024-06-24 19:58:46 +0530

    created  get users api route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -8,60 +8,29 @@ export const test = (req, res) => {
 
 export const updateUser = async (req, res, next) => {
 	if (req.user.id !== req.params.userId) {
-		return next(
-			errorHandler(
-				403,
-				"you are not allowed to update this user"
-			)
-		);
+		return next(errorHandler(403, "you are not allowed to update this user"));
 	}
 	if (req.body.password) {
 		if (req.body.password.length < 6) {
-			return next(
-				errorHandler(
-					400,
-					"password must be at least 6 characters"
-				)
-			);
+			return next(errorHandler(400, "password must be at least 6 characters"));
 		}
-		req.body.password = bcryptjs.hashSync(
-			req.body.password,
-			10
-		);
+		req.body.password = bcryptjs.hashSync(req.body.password, 10);
 	}
 	if (req.body.username) {
-		if (
-			req.body.username.length < 7 ||
-			req.body.username.length > 20
-		) {
+		if (req.body.username.length < 7 || req.body.username.length > 20) {
 			return next(
-				errorHandler(
-					400,
-					"username must be between 7 and 20 characters"
-				)
+				errorHandler(400, "username must be between 7 and 20 characters")
 			);
 		}
 		if (req.body.username.includes(" ")) {
-			return next(
-				errorHandler(
-					400,
-					"username must not contain spaces"
-				)
-			);
+			return next(errorHandler(400, "username must not contain spaces"));
 		}
-		if (
-			req.body.username !== req.body.username.toLowerCase()
-		) {
-			return next(
-				errorHandler(400, "username must be lowercase")
-			);
+		if (req.body.username !== req.body.username.toLowerCase()) {
+			return next(errorHandler(400, "username must be lowercase"));
 		}
 		if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
 			return next(
-				errorHandler(
-					400,
-					"username must only contain letters and numbers"
-				)
+				errorHandler(400, "username must only contain letters and numbers")
 			);
 		}
 	}
@@ -87,12 +56,7 @@ export const updateUser = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
 	if (req.user.id !== req.params.userId) {
-		return next(
-			errorHandler(
-				403,
-				"you are not allowed to delete this user"
-			)
-		);
+		return next(errorHandler(403, "you are not allowed to delete this user"));
 	}
 	try {
 		await User.findByIdAndDelete(req.params.userId);
@@ -108,6 +72,44 @@ export const signout = (req, res, next) => {
 			.clearCookie("access_token")
 			.status(200)
 			.json("user has been signed out");
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const getUsers = async (req, res, next) => {
+	if (!req.user.isAdmin) {
+		return next(errorHandler(403, "you are not allowed to see all user"));
+	}
+	try {
+		const startIndex = parseInt(req.query.startIndex) || 0;
+		const limit = parseInt(req.query.limit) || 10;
+		const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+		const users = await User.find()
+			.sort({ createdAt: sortDirection })
+			.skip(startIndex)
+			.limit(limit);
+		const usersWithoutPassword = users.map((user) => {
+			const { password, ...rest } = user._doc;
+			return rest;
+		});
+
+		const totalUsers = await User.countDocuments();
+		const now = new Date();
+		const oneMonthAgo = new Date(
+			now.getFullYear(),
+			now.getMonth() - 1,
+			now.getDate()
+		);
+		const lastMonthUsers = await User.countDocuments({
+			createdAt: { $gte: oneMonthAgo },
+		});
+		res.status(200).json({
+			users: usersWithoutPassword,
+			totalUsers,
+			lastMonthUsers,
+		});
 	} catch (error) {
 		next(error);
 	}
