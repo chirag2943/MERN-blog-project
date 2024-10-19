e76d5f36d9ab35196716269973c0f5c75819b20e
chirag2943 <chiragchouhan866@gmail.com>
2024-10-19 17:42:46 +0530

    Added search functionality to the website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
 import ScrollToTop from "./components/ScrollToTop";
+import Search from "./pages/Search";
 
 export function App() {
   return (
@@ -33,6 +34,7 @@ export function App() {
         </Route>
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
       <Footer />
     </BrowserRouter>
