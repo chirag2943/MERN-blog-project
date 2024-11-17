2d74f7004eae47e88f58f6c70f06ba819368cd11
chirag2943 <chiragchouhan866@gmail.com>
2024-11-17 23:51:36 +0530

    completed individual project page styles

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,106 +1,119 @@
 import { Footer } from "flowbite-react";
 import { Link } from "react-router-dom";
 import {
-	BsFacebook,
-	BsInstagram,
-	BsTwitter,
-	BsGithub,
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsGithub,
+  BsLinkedin,
 } from "react-icons/bs";
 
 export default function FooterCom() {
-	return (
-		<Footer
-			container
-			className="border border-t-8 border-teal-700 dark:bg-[rgb(55,59,66)]">
-			<div className="w-full max-w-7xl mx-auto">
-				<div className="grid w-full justify-between sm:flex md:grid-cols-1">
-					<div className="mt-5">
-						<Link
-							to="/"
-							className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white">
-							<span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-								Chirag s{" "}
-							</span>
-							Blog
-						</Link>
-					</div>
-					<div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
-						<div>
-							<Footer.Title title="About" />
-							<Footer.LinkGroup col>
-								<Footer.Link
-									href="#"
-									target="_blank"
-									rel="noopener noreferrer">
-									Amazon clone
-								</Footer.Link>
-							</Footer.LinkGroup>
-							<Footer.LinkGroup col>
-								<Footer.Link
-									href="#"
-									target="_blank"
-									rel="noopener noreferrer">
-									React-project
-								</Footer.Link>
-							</Footer.LinkGroup>
-						</div>
-						<div>
-							<Footer.Title title="Follow me" />
-							<Footer.LinkGroup col>
-								<Footer.Link
-									href="#"
-									target="_blank"
-									rel="noopener noreferrer">
-									GitHub
-								</Footer.Link>
-							</Footer.LinkGroup>
-							<Footer.LinkGroup col>
-								<Footer.Link
-									href="#"
-									target="_blank"
-									rel="noopener noreferrer">
-									LinkedIn
-								</Footer.Link>
-							</Footer.LinkGroup>
-						</div>
-						<div>
-							<Footer.Title title="Contact us" />
-							<Footer.LinkGroup col>
-								Contact number
-							</Footer.LinkGroup>
-							<Footer.LinkGroup col>
-								<Footer.Link>Gmail</Footer.Link>
-							</Footer.LinkGroup>
-						</div>
-					</div>
-				</div>
-				<Footer.Divider />
-				<div className="w-full sm:flex sm:items-center sm:justify-between">
-					<Footer.Copyright
-						href="#"
-						by="Chirag's blog"
-						year={new Date().getFullYear()}
-					/>
-					<div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
-						<Footer.Icon
-							href="#"
-							icon={BsFacebook}
-						/>
-						<Footer.Icon
-							href="#"
-							icon={BsInstagram}
-						/>
-						<Footer.Icon
-							href="#"
-							icon={BsGithub}
-						/>
-						<Footer.Icon
-							href="#"
-							icon={BsTwitter}
-						/>
-					</div>
-				</div>
-			</div>
-		</Footer>
-	);
+  return (
+    <Footer
+      container
+      className="border border-t-8 border-teal-700 dark:bg-[rgb(55,59,66)]"
+    >
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Chirag s{" "}
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="Projects" className=" cursor-default " />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/chirag2943/amazon-clone"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Amazon clone
+                </Footer.Link>
+              </Footer.LinkGroup>
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/chirag2943/react_project"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  React-project
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow me" className=" cursor-default " />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/chirag2943?tab=overview&from=2024-11-01&to=2024-11-17"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </Footer.Link>
+              </Footer.LinkGroup>
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.linkedin.com/in/chirag-chouhan-29april"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Contact me" className=" cursor-default " />
+              <Footer.LinkGroup col>
+                <Footer.Link href="https://wa.me/918073919349" target="_blank">
+                  Contact number
+                </Footer.Link>
+              </Footer.LinkGroup>
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="mailto:chiragchouhan866@gmail.com"
+                  target="_blank"
+                >
+                  Gmail
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="/"
+            by="Chirag's blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon
+              href="https://www.instagram.com/itss_chirag.__/"
+              target="_BLANK"
+              icon={BsInstagram}
+            />
+            <Footer.Icon
+              href="https://github.com/chirag2943?tab=overview&from=2024-11-01&to=2024-11-17"
+              target="_BLANK"
+              icon={BsGithub}
+            />
+            <Footer.Icon
+              href="https://www.linkedin.com/in/chirag-chouhan-29april"
+              target="_BLANK"
+              icon={BsLinkedin}
+            />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
 }
--- client/src/pages/ProjectPage.jsx
+++ client/src/pages/ProjectPage.jsx
@@ -1,5 +1,6 @@
+import { Button } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function ProjectPage() {
   const { projectSlug } = useParams();
@@ -13,10 +14,6 @@ export default function ProjectPage() {
         if (res.ok) {
           //this is for showing individual projects
           setProject(data.filter((project) => project.slug === projectSlug)[0]);
-
-          // console.log(
-          //   data.filter((project) => project.slug === projectSlug)[0]
-          // );
         }
       } catch (error) {
         console.log(error.message);
@@ -26,9 +23,46 @@ export default function ProjectPage() {
   }, [projectSlug]);
 
   return (
-    <div>
-      <h1>{project.title}</h1>
-      <p>{project.desc}</p>
-    </div>
+    <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen ">
+      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+        {project && project.title}
+      </h1>
+      <img
+        src={project && project.image}
+        alt={project && project.image}
+        className="mt-10 p-3 max-h-[600px] w-full object-cover"
+      />
+      <div className="mr-10">
+        <Link to="https://github.com/chirag2943/amazon-clone" target="_blank">
+          <Button
+            gradientDuoTone="purpleToBlue"
+            className="float-right w-72"
+            outline
+          >
+            <div className="flex items-center gap-2">
+              <svg
+                className="w-8 h-8 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <span>open github repo</span>
+            </div>
+          </Button>
+        </Link>
+      </div>
+      <div className="p-3 max-w-4xl mx-auto w-full post-content">
+        {project && project.desc}
+      </div>
+    </main>
   );
 }
